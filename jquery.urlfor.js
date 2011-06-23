5e516051aba5f685c5d2ae88d7623818d13ba745
oroce <robert@oroszi.net>
2011-06-23 23:54:41 +0200

    readme updated

--- jquery.urlfor.js
+++ jquery.urlfor.js
@@ -1,30 +1,30 @@
 (function($) {
-	$.urlFor = function(url, params) {
+	$.urlFor = function( url, params ) {
 		if( url && /\w+\/\/:/.test( url ) ){
 			return url; //absolute
 		}
-		var isArr = $.isArray(params);
+		var isArr = $.isArray( params );
 			
 		if (arguments.length === 1) {
 			return url; //url was passed
 		}
-		else if (url && typeof params === "string") {
-			return Array.prototype.slice.call( arguments ).join("/"); //list was passed
+		else if ( url && typeof params === "string" ) {
+			return Array.prototype.slice.call( arguments ).join( "/" ); //list was passed
 		}
-		else if (url && ($.isPlainObject(params) || isArr)) { //placeholder with object was passed (array, hash)
+		else if ( url && ( $.isPlainObject( params ) || isArr ) ){ //placeholder with object was passed (array, hash)
 			var newUrl = [],
 				index = 0;
-			$.each(url.split("/"), function(i, elem) {
-				newUrl.push((elem || "").replace(/:(\w+)/, function(str, match, ret) {
-					if (match) {
+			$.each( url.split( "/" ), function( i, elem ){
+				newUrl.push( ( elem || "" ).replace( /:(\w+)/, function( str, match ) {
+					if ( match){
 						return ( isArr ? params[ index++ ] : params[ match ] ) || ""; 
 					}
-					else {
+					else{
 						return "";
 					}
 				}));
 			});
-			return newUrl.join("/");
+			return newUrl.join( "/" );
 		}
 	}
 })(jQuery);
